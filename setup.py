81bbc1e05ebe15ae68435789eab2a05712dd4241
loevliedenny@gmail.com <59749099+loevlie@users.noreply.github.com>
2021-03-23 11:27:41 -0400

    Finishing Pypi setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md","r") as fh:
     long_description = fh.read()
 
 setup(name='nb_search',
-      version='1.0.0',
+      version='1.0.2',
       description='Module for searching through Jupyter Notebooks in a provided directory',
       long_description=long_description,
       long_description_content_type="text/markdown",
